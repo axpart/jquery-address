113adc46ca19428dda8477029139b17d7c2dbbb9
Bartek Szopka <bartek.szopka+github@gmail.com>
2010-03-23 13:44:51 +0100

    tests made synchronous

--- test/test.js
+++ test/test.js
@@ -1,88 +1,67 @@
-asyncTest("Value test", function() {
-    setTimeout(function() {
-        $.address.value('/test');
-        equals($.address.value(), '/test');
-        start();
-    }, 1000);
+test("Value test", function() {
+    $.address.value('/test');
+    equals($.address.value(), '/test');
 });
 
-asyncTest("Query test", function() {
-    setTimeout(function() {
-        $.address.queryString('p=0');
-        equals($.address.value(), '/test?p=0');
-        equals($.address.path(), '/test');
-        equals($.address.queryString(), 'p=0');
-        start();
-    }, 1000);
+test("Query test", function() {
+    $.address.queryString('p=0');
+    equals($.address.value(), '/test?p=0');
+    equals($.address.path(), '/test');
+    equals($.address.queryString(), 'p=0');
 });
 
-asyncTest("Parameter test", function() {
-    setTimeout(function() {
-        $.address.parameter('p', 1);
-        equals($.address.value(), '/test?p=1');
-        equals($.address.queryString(), 'p=1');
-        equals($.address.parameter('p'), '1');
-        start();
-    }, 1000);
+test("Parameter test", function() {
+    $.address.parameter('p', 1);
+    equals($.address.value(), '/test?p=1');
+    equals($.address.queryString(), 'p=1');
+    equals($.address.parameter('p'), '1');
 });
 
-asyncTest("Parameter test", function() {
-    setTimeout(function() {
-        $.address.parameter('p', 2, true);
-        equals($.address.value(), '/test?p=1&p=2');
-        equals($.address.queryString(), 'p=1&p=2');
-        equals($.address.parameter('p').toString(), '1,2');
-        start();
-    }, 1000);
+test("Parameter test", function() {
+    $.address.parameter('p', 2, true);
+    equals($.address.value(), '/test?p=1&p=2');
+    equals($.address.queryString(), 'p=1&p=2');
+    equals($.address.parameter('p').toString(), '1,2');
 });
 
-asyncTest("Parameter test", function() {
-    setTimeout(function() {
-        $.address.autoUpdate(false)
-        	.queryString('')
-            .parameter('p', 1, true)
-            .parameter('p', 2)
-            .parameter('p', 3, true)
-            .parameter('s', 1)
-            .parameter('s', 2, true)
-            .parameter('s', 3)
-            .parameter('t', 0)
-            .parameter('t', null)
-	        .autoUpdate(true)
-	        .update();
-        equals($.address.value(), '/test?p=2&p=3&s=3');
-        equals($.address.parameter('p').toString(), '2,3');
-        equals($.address.parameter('s').toString(), 3);
-        equals($.address.parameterNames().toString(), 'p,s');
-        start();
-    }, 1000);
+test("Parameter test", function() {
+    $.address.autoUpdate(false)
+        .queryString('')
+        .parameter('p', 1, true)
+        .parameter('p', 2)
+        .parameter('p', 3, true)
+        .parameter('s', 1)
+        .parameter('s', 2, true)
+        .parameter('s', 3)
+        .parameter('t', 0)
+        .parameter('t', null)
+        .autoUpdate(true)
+        .update();
+    equals($.address.value(), '/test?p=2&p=3&s=3');
+    equals($.address.parameter('p').toString(), '2,3');
+    equals($.address.parameter('s').toString(), 3);
+    equals($.address.parameterNames().toString(), 'p,s');
 });
 
-asyncTest("Fragment test with params", function() {
-    setTimeout(function() {
-        $.address.fragment('fragment');
-        equals($.address.value(), '/test?p=2&p=3&s=3#fragment');
-        equals($.address.path(), '/test');
-        equals($.address.parameter('p').toString(), '2,3');
-        equals($.address.parameter('s').toString(), 3);
-        equals($.address.parameterNames().toString(), 'p,s');
-        equals($.address.fragment(), 'fragment');
-        start();
-    }, 1000);
+test("Fragment test with params", function() {
+    $.address.fragment('fragment');
+    equals($.address.value(), '/test?p=2&p=3&s=3#fragment');
+    equals($.address.path(), '/test');
+    equals($.address.parameter('p').toString(), '2,3');
+    equals($.address.parameter('s').toString(), 3);
+    equals($.address.parameterNames().toString(), 'p,s');
+    equals($.address.fragment(), 'fragment');
 });
 
-asyncTest("Fragment test", function() {
-    setTimeout(function() {
-        $.address.value('/test');
-        $.address.fragment('fragment');
-        equals($.address.value(), '/test#fragment');
-        equals($.address.path(), '/test');
-        equals($.address.fragment(), 'fragment');
-        start();
-    }, 1000);
+test("Fragment test", function() {
+    $.address.value('/test');
+    $.address.fragment('fragment');
+    equals($.address.value(), '/test#fragment');
+    equals($.address.path(), '/test');
+    equals($.address.fragment(), 'fragment');
 });
 
-
-setTimeout(function() {
+QUnit.done = function() {
     $.address.value('/');
-}, 10000);
+};
+
